db1c24ddc553ddea5335032d8136bbc1ebc2256c
MotiveKo <rhehdrla@naver.com>
2021-11-30 23:34:09 +0900

    WebComponent - modal 레이아웃

--- Javascript-Study/WebComponent/Modal/src/modal.ts
+++ Javascript-Study/WebComponent/Modal/src/modal.ts
@@ -11,10 +11,58 @@ export class Modal extends HTMLElement {
           width: 100%;
           height: 100vh;
           background-color: rgba(0,0,0,0.75);   
+          z-index: 10;
+        }
+        #modal {
+          z-index : 100;
+          position: fixed;
+          left: 25%;
+          top: 15vh;
+          width: 50%;
+          background: white;
+          border-radius: 3px;
+          box-shadow: 0 2px 8px rgba(0,0,0,0.26);
+
+          display: flex;
+          flex-direction: column;
+          justify-content: space-around;
+
+        }
+
+        header {
+          padding: 1rem;
+        }
+        header h2 {
+          font-size: 1.25rem;
+        }
+
+        #main {
+          padding: 1rem;
+        }
+
+        #actions {
+          padding: 1rem;
+          border-top: 1px solid lightgrey;
+          display: flex;
+          justify-content: flex-end
+        }
+        #actions button {
+          margin: 0 0.25rem
         }
       </style>
       <div id="backdrop"></div>
-      <div id="modal"></div>
+      <div id="modal">
+        <header>
+          <h2>Please Confirm</h2>        
+        </header>
+        <section id="main">
+          <slot></slot>
+        </section>
+        <section id="actions">
+          <button>Cancel</button>
+          <button>Okay</button>
+        </section>
+      </div>
     `;
   }
 }
