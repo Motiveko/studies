61ef6753ea94f5f83685ed602645c4317a46e153
MotiveKo <rhehdrla@naver.com>
2021-12-19 23:14:34 +0900

    테스트 환경 jest로 변경중..

--- Javascript-Study/Frameworkless Frontend Development/src/view/todo.spec.ts
+++ Javascript-Study/Frameworkless Frontend Development/src/view/todo.spec.ts
@@ -1,6 +1,35 @@
-describe('테스트입니다', () => {
-  console.log('잘 작동하나요???');
-  it('테스트유', () => {
-    expect(1).toBe(1);
+import todosView from './todos';
+
+let targetElement: Element | undefined;
+
+describe('filtersView', () => {
+  beforeEach(() => {
+    targetElement = document.createElement('ul');
   });
+
+  test('모든 todo에 대해 li요소를 만든다 ', () => {
+    if (!targetElement) {
+      throw new Error('targetElement가 생성되지 않았습니다');
+    }
+    const newTodoList = todosView(targetElement, {
+      currentFilter: '',
+      todos: [
+        {
+          text: 'First',
+          completed: true
+        },
+        {
+          text: 'Second',
+          completed: true
+        },
+        {
+          text: 'Third',
+          completed: false
+        }
+      ]
+    });
+    expect(newTodoList.querySelectorAll('li').length).toBe(3);
+  });
+
+  test('');
 });
