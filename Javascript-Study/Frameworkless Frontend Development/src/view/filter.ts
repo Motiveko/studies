562c5cd41d9ea507c5fd4b433b65f61b8325fd97
MotiveKo <rhehdrla@naver.com>
2021-12-22 00:15:32 +0900

    test: 테스트코드 작성 및 prettier 설정에 따른 코드 리포맷

--- Javascript-Study/Frameworkless Frontend Development/src/view/filter.ts
+++ Javascript-Study/Frameworkless Frontend Development/src/view/filter.ts
@@ -1,14 +1,14 @@
 import { State } from '..';
 
 export default (target: Element, state: State) => {
-  const newCounter = target.cloneNode(true) as Element;
-  const { currentFilter } = state;
-  Array.from(newCounter.querySelectorAll('li a')).forEach(a => {
-    if (a.textContent === currentFilter) {
-      a.classList.add('selected');
-    } else {
-      a.classList.remove('selected');
-    }
-  });
-  return newCounter;
+	const newCounter = target.cloneNode(true) as Element;
+	const { currentFilter } = state;
+	Array.from(newCounter.querySelectorAll('li a')).forEach(a => {
+		if (a.textContent === currentFilter) {
+			a.classList.add('selected');
+		} else {
+			a.classList.remove('selected');
+		}
+	});
+	return newCounter;
 };
