b0561473ba484620230209f30971c55782b575bc
MotiveKo <rhehdrla@naver.com>
2022-02-05 22:22:02 +0900

    first commit - eslint, prettier 적용 - firebase appkey 설정 및 hosting 설정 - bootstrap 설치

---
+++ React-Study/Projects/devlog/README.md
@@ -0,0 +1,6 @@
+# devlog
+velog같은 블로그를 리액트로 한번 만들어보자
+
+주소 : https://devlog-67e42.web.app/
+
+개발중..
---
+++ React-Study/Projects/devlog/src/App.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function App() {
+  return <div>배포좀 해줘!!!!!!!!!!</div>;
+}
+
+export default App;
