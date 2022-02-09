52b997dc8cbd87e452bbcd4579de57174a3d0987
motiveko <rhehdrla@naver.com>
2022-02-09 16:12:02 +0900

    feat: date util 구현

---
+++ React-Study/Projects/devlog/src/utils/date-utils.spec.ts
@@ -0,0 +1,32 @@
+import { parseDate } from './date-utils';
+const oneMinuteMillis = 1_000 * 60;
+const oneHourMillis = oneMinuteMillis * 60;
+const oneDayMillis = oneHourMillis * 24;
+const oneWeekMillis = oneDayMillis * 7;
+
+describe('date util 테스트', () => {
+  test('테스트', () => {
+    const nowMillis = Date.now();
+
+    // 1. 방금
+    expect(parseDate(nowMillis - 30)).toBe('방금 전');
+
+    // 2. ~ 분 전
+    expect(parseDate(nowMillis - 5 * oneMinuteMillis)).toBe('5분 전');
+
+    // 3. ~ 시간 전
+    [...Array(23).keys()].map(i => i + 1).forEach(i => expect(parseDate(nowMillis - i * oneHourMillis)).toBe(`${i}시간 전`));
+
+    // 4. ~ 일 전
+    [...Array(6).keys()].map(i => i + 1).forEach(i => expect(parseDate(nowMillis - i * oneDayMillis)).toBe(`${i}일 전`));
+
+    // 5. mm/dd - 올해까진 MM/DD로 표시한다. 만약에 현재 날짜가 1월 7일 이전이면 이건 표시될 수 없고, ~ 일 전이 표시된다.
+    const now = new Date();
+    const januarySecond = new Date(now.getFullYear(), 0, 2);
+    expect(parseDate(januarySecond.getTime())).toBe('1/2');
+
+    // yyyy/mm/dd 출력 테스트
+    const anotherYearDate = '2020/4/1';
+    expect(parseDate(new Date(anotherYearDate).getTime())).toBe(anotherYearDate);
+  });
+});
---
+++ React-Study/Projects/devlog/src/utils/date-utils.ts
@@ -0,0 +1,49 @@
+const oneMinuteMillis = 1_000 * 60;
+const oneHourMillis = oneMinuteMillis * 60;
+const oneDayMillis = oneHourMillis * 24;
+const oneWeekMillis = oneDayMillis * 7;
+
+/**
+ * millisecond를 현재시간을 기준으로 아래 포맷으로 파싱한다.
+ * 1분 이내 : 방금 전
+ * 1시간 이내 : ~분 전
+ * 24시간 이내 : ~시간 전
+ * 24시간 ~ 1주일 이내 : ~일 전
+ * 1주일 ~ 올해 이내 : MM/dd
+ * 작년 이전 : YYYY/MM/dd
+ * @param millis string
+ */
+type ParseDate = (millis: number) => string;
+
+export const parseDate: ParseDate = millis => {
+  const nowDate = new Date();
+  const nowMillis = nowDate.getTime();
+
+  const timeGap = nowMillis - millis;
+  if (timeGap <= oneMinuteMillis) {
+    return '방금 전';
+  }
+
+  if (timeGap <= oneHourMillis) {
+    return Math.floor(timeGap / oneMinuteMillis) + '분 전';
+  }
+
+  if (timeGap <= oneDayMillis) {
+    return Math.floor(timeGap / oneHourMillis) + '시간 전';
+  }
+
+  if (timeGap <= oneWeekMillis) {
+    return Math.floor(timeGap / oneDayMillis) + '일 전';
+  }
+
+  const date = new Date(millis);
+  const year = date.getFullYear();
+  const month = date.getMonth() + 1;
+  const day = date.getDate();
+
+  if (nowDate.getFullYear() === year) {
+    return `${month}/${day}`;
+  }
+
+  return `${year}/${month}/${day}`;
+};
