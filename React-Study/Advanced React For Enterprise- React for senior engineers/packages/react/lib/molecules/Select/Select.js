ce9be30ac1791261a1c3c1c33304d3e23d82283c
motiveko <58010269+Motiveko@users.noreply.github.com>
2022-05-18 23:41:32 +0900

    feat(@ds.e/react): Select 컴포넌트

--- React-Study/Advanced React For Enterprise- React for senior engineers/packages/react/lib/molecules/Select/Select.js
+++ React-Study/Advanced React For Enterprise- React for senior engineers/packages/react/lib/molecules/Select/Select.js
@@ -1,14 +1,17 @@
-import React, { useState, useRef, useEffect } from 'react';
+import React, { useRef, useState, useEffect, useMemo } from 'react';
+import Text from '../../atoms/Text/Text.js';
 
 const Select = ({ options = [], label = "Please select an option ...", onOptionSelected: handler, }) => {
-    const [isOpen, setIsOpen] = useState(false);
     const labelRef = useRef(null);
     const [overlayTop, setOverlayTop] = useState(undefined);
     useEffect(() => {
         setOverlayTop((labelRef.current?.offsetHeight || 0) + 10);
     }, [labelRef.current?.offsetHeight]);
+    const [isOpen, setIsOpen] = useState(false);
+    const [selectedIndex, setSelectedIndex] = useState(undefined);
     const onOptionSelected = (option, optionIndex) => {
-        setIsOpen(!isOpen);
+        setIsOpen(false);
+        setSelectedIndex(optionIndex);
         if (handler) {
             handler(option, optionIndex);
         }
@@ -16,13 +19,24 @@ const Select = ({ options = [], label = "Please select an option ...", onOptionS
     const onLabelClick = () => {
         setIsOpen(!isOpen);
     };
+    const selectedOption = useMemo(() => (selectedIndex === undefined ? label : options[selectedIndex].label), [options, selectedIndex]);
     return (React.createElement("div", { className: "dse-select" },
         React.createElement("button", { className: "dse-select__label", onClick: () => onLabelClick(), ref: labelRef },
-            React.createElement("span", null, label),
-            React.createElement("svg", { xmlns: "http://www.w3.org/2000/svg", className: "h-6 w-6", fill: "none", viewBox: "0 0 24 24", stroke: "currentColor", width: "1rem", height: "1rem", strokeWidth: 2 },
-                React.createElement("path", { strokeLinecap: "round", strokeLinejoin: "round", d: "M19 9l-7 7-7-7" }))),
-        isOpen && (React.createElement("ul", { style: { top: overlayTop }, className: "dse-select__overlay" }, options.map((option, optionIndex) => (React.createElement("li", { onClick: () => onOptionSelected(option, optionIndex), key: option.value }, option.label)))))));
+            React.createElement(Text, null, selectedOption),
+            caretIcon(isOpen)),
+        isOpen && (React.createElement("ul", { style: { top: overlayTop }, className: "dse-select__overlay" }, options.map((option, optionIndex) => {
+            const isSelected = selectedIndex === optionIndex;
+            return (React.createElement("li", { className: `dse-select__option ${isSelected ? "dse-select__option--selected" : ""}`, onClick: () => onOptionSelected(option, optionIndex), key: option.value },
+                React.createElement(Text, null, option.label),
+                isSelected && checkIcon));
+        })))));
 };
+const caretIcon = (isOpen) => {
+    return (React.createElement("svg", { xmlns: "http://www.w3.org/2000/svg", className: `h-6 w-6 dse-select__caret ${isOpen ? "dse-select__caret--open" : "dse-select__caret--closed"}`, fill: "none", viewBox: "0 0 24 24", stroke: "currentColor", width: "1rem", height: "1rem", strokeWidth: 2 },
+        React.createElement("path", { strokeLinecap: "round", strokeLinejoin: "round", d: "M19 9l-7 7-7-7" })));
+};
+const checkIcon = (React.createElement("svg", { xmlns: "http://www.w3.org/2000/svg", className: "h-6 w-6", fill: "none", viewBox: "0 0 24 24", stroke: "currentColor", width: "1rem", height: "1rem", strokeWidth: 2 },
+    React.createElement("path", { strokeLinecap: "round", strokeLinejoin: "round", d: "M5 13l4 4L19 7" })));
 
 export { Select as default };
 //# sourceMappingURL=Select.js.map
