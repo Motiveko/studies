e6566dee72c4e67732a1cd3b07244d5f8a99504e
motiveko <58010269+Motiveko@users.noreply.github.com>
2022-05-24 22:59:34 +0900

    Revert "~34. Select box calculate overlay position까지"
    This reverts commit e0de4556ec33bb494c202b1b9ba66c5a2a108257.

--- React-Study/Advanced React For Enterprise- React for senior engineers/packages/react/lib/index.d.ts
+++ React-Study/Advanced React For Enterprise- React for senior engineers/packages/react/lib/index.d.ts
@@ -1,6 +1,5 @@
 import Color from "./atoms/Color";
 import Text from "./atoms/Text";
 import Margin from "./atoms/Margin";
-import Select from "./molecules/Select";
-export { Color, Text, Margin, Select };
+export { Color, Text, Margin };
 //# sourceMappingURL=index.d.ts.map
