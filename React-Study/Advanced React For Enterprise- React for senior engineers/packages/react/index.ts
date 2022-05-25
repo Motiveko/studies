14bc5770ca03060e55ac416b496d1370841f6cc0
motiveko <58010269+Motiveko@users.noreply.github.com>
2022-05-25 23:05:16 +0900

    test: react, foundation 패키지 unit 테스트 작성

---
+++ React-Study/Advanced React For Enterprise- React for senior engineers/packages/react/index.ts
@@ -0,0 +1,5 @@
+import Color from "./src/atoms/Color";
+import Text from "./src/atoms/Text";
+import Margin from "./src/atoms/Margin";
+import Select from "./src/molecules/Select";
+export { Color, Text, Margin, Select };
